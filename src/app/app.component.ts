9b15d4f7470e6f8687cf4399fd357d46991a773f
jeanvargblz <jeanvargblz@gmail.com>
2018-11-13 19:05:42 -0500

    arreglando poligonos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -25,12 +25,37 @@ export class AppComponent {
   siguiendoA: string = null;
   siguiendoNombre: string = null;
 
-  paths: Array<LatLngLiteral> = [
-   {lat: -16.3988900 , lng:-71.5350000},
-   {lat:-16.3788910 , lng:-71.5450010},
-   {lat:-16.3888920 , lng:-71.5550020},
-   {lat:-16.3688930 , lng:-71.5650030}
-];
+nestedPaths: Array<Array<LatLngLiteral>> = [
+[
+  {lat:-16.35648,lng: -71.56882},
+  {lat:-16.35668,lng: -71.54762},
+  {lat:-16.38891,lng: -71.5477},
+  {lat:-16.38925,lng: -71.56863}
+],[
+  {lat:-16.38925,lng: -71.56863},
+  {lat:-16.38891,lng: -71.5477},
+  {lat:-16.39295,lng: -71.53863}, 
+  {lat:-16.40806,lng: -71.54651},
+  {lat:-16.40506,lng: -71.56828}
+],[
+  {lat:-16.39295,lng: -71.53863},
+  {lat:-16.40134,lng: -71.5279},
+  {lat:-16.41056,lng: -71.53075},
+  {lat:-16.41122,lng: -71.54163},
+  {lat:-16.40395,lng: -71.54441}
+
+],[
+  {lat:-16.3737,lng: -71.51273},
+  {lat:-16.3735,lng: -71.49789},
+  {lat:-16.38666,lng: -71.49759},
+  {lat:-16.38644,lng: -71.5127}
+],[
+  {lat:-16.38658,lng: -71.50339},
+  {lat:-16.38674,lng: -71.4877},
+  {lat:-16.40291,lng: -71.48739},
+  {lat:-16.40266,lng: -71.50318}
+]];
+
 
   constructor(db: AngularFirestore) {
     db.collection('usuarios').valueChanges()
